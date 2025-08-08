81460e1570e65e7ef4af7cf7ec5c9cd869f2c78a
Lucas <lucasguimaraes.reis@estudante.ufjf.br>
2025-08-08 13:41:17 -0300

    fiz a mecanica de rodada e tela de vitória

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,18 @@
 //importações
 import "./style.css"; //Pra usar os estilos de forma global
+import "./estilos/barra-lateral.css"
+import "./estilos/botoes.css"
+import "./estilos/modal.css"
 import "./components/carta-component"; //Importa o componente de carta
 import { criarBaralho, pegarCarta } from './Importados/carta_baralho.js';
-import { jogacarta, descarta, inicializarEstado, Monitoramento } from './Importados/jogo.js';
-import { renderiza, atualizarBaralhoContagem, atualizaMeta, atualizarPontuacaoInterface } from './Importados/interface.js';
+import { inicializarEstado, Monitoramento, inicializarBotoes } from './Importados/jogo.js';
+import { renderiza, atualizarBaralhoContagem} from './Importados/interface.js';
 
 
 /**********************************************************************
  CONSTANTES
 ************************************************************************/
-let nmrJogadas = 4;
-let nmrDescartes = 3;
-const botjoga = document.getElementById('botaojoga');
-const botdesc = document.getElementById('botaodescarta');
 
-//faz os botões chamarem as funções correspondentes
-botjoga?.addEventListener('click', ()=>{
-  if(nmrJogadas>0){
-    jogacarta();
-      nmrJogadas-=1;
-  }else{
-    botjoga.classList.add('acabou');
-  }
-});
-botdesc?.addEventListener('click', () => {
-  if(nmrDescartes>0){
-    descarta()
-    nmrDescartes-=1;
-  }
-  botdesc.classList.add('acabou')
-  }
-);
 
 /**********************************************************************
 FUNÇÃO QUE INICIA TUDO
@@ -42,7 +24,7 @@ function iniciarJogo(): void {
   
   // Inicializa o estado do jogo
   inicializarEstado(baralho, mao);
-  
+  inicializarBotoes();
   // Renderiza as cartas na interface
   renderiza(mao);
   
