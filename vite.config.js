64ac059d6408bd9ce9cb1f0b9e49ccafd2838802
Lucas <lucasguimaraes.reis@estudante.ufjf.br>
2025-08-19 21:04:26 -0300

    arrumadno execução no github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 
-// Defina o nome do seu repositório aqui
-const repoName = 'ufjf-dcc206-2025-1-a-trb-canastra';
 
 export default defineConfig({
   // O caminho base da sua aplicação para o GitHub Pages
-  base: `/${repoName}/`,
+  base: `/ufjf-dcc206-2025-1-a-trb-canastra/`,
   plugins: [], // Se você tiver plugins, mantenha-os aqui
 })
